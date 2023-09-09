e029c9705ef0b527079b25f5259f4ec06b3a9325
faguilarleal <agu22243@uvg.edu.gt>
2023-09-08 22:49:18 -0600

    .

--- primes3.cpp
+++ primes3.cpp
@@ -2,7 +2,7 @@
  *----------------------------------------
  * UNIVERSIDAD DEL VALLE DE GUATEMALA
  * CC3086 - Programacion de Microprocesadores
- * Modificado por: KB
+ * Modificado por: Francis y Angela :D
  * Fecha: 2023/09/03
  * Referencias:
  * "Predicates" - The C++ Standard Library (Josuttis)
@@ -17,130 +17,116 @@
  * Implementación simplista usando pthreads
  * ----------------------------------------
  */
-
-#include <iostream>     //cout, cin, endl
-#include <cmath> 		//pow
-#include <pthread.h>	//pthread_
-#include <ctime>		//time_t, clock, difftime	
+#include <iostream>
+#include <cmath>
+#include <pthread.h>
+#include <ctime>
 
 using namespace std;
 
-#define THREADS 4
-
-//Prototipo función identificadora
+// Prototipo función identificadora
 bool isPrime(int number);
 
-//Prototipo rutina para hilos
-void *primeTest (void *thisNumber);
+// Prototipo rutina para hilos
+void *primeTest(void *threadData);
 
-/*
- * MAIN
- */
-int main()
-{
-	int exponente, numThreads = THREADS;
+struct ThreadData {
+    long start;
+    long end;
+};
+
+int main() {
+    int exponente, numThreads;
 
     // Contadores para el reloj
-	clock_t start_t, end_t;
-	double total_t;
-	
-	cout<<"-----------------------------------------------\n";
-	cout<<"Impresión de los numeros primos menores a 10^n\n";
-	cout<<"-----------------------------------------------\n";
-	cout<<"Ingrese el valor del exponente para base 10: ";
+    clock_t start_t, end_t;
+    double total_t;
 
-    //Lectura desde stdin de un valor segun el tipo de la variable destino
-	cin>>exponente;
-	
-	cout<<"Ingrese el numero de threads: ";
-	cin>>numThreads;
-	
-	long limit = pow(10,exponente);
-	
-	cout<<"Buscando primos entre 0 y "<<limit<<"...";
-	cout<<endl;
-	
-	long i = 0;
-	long j = 0; 		//Global
-	int rc;
-	
-    //Definicion del objeto pthread
-	pthread_t threadID[numThreads];
-	
-    //Contador de inicio en ticks de reloj
-	start_t = clock();
-	
-    //Recoremos todo el dominio de numeros 10^n
-	for(i=0; i<limit;)
-	{
-		//Asignamos alternamente numeros a cada hilo
-        for(j = 0; j<numThreads; j++)	
-		{
-			rc = pthread_create(&threadID[j],NULL,primeTest,(void *)i);
-			if(rc)
-			{
-				cout<<"No se pudo crear el hilo "<<j<<endl;
-				exit(-1);
-			}
-			i++;
-		}
-	}
-	
-	for(j = 0; j<numThreads; j++)	
-	{
-		rc = pthread_join(threadID[j],NULL);
-		if(rc)
-		{
-			cout<<"No se pudo unir el hilo "<<j<<endl;
-			exit(-1);
-		}
-	}
-	
-	end_t = clock();
-	total_t = difftime(end_t,start_t)/CLOCKS_PER_SEC;
-	cout<<"Total time: "<<total_t<<endl;
+    cout << "-----------------------------------------------\n";
+    cout << "Impresión de los numeros primos menores a 10^n\n";
+    cout << "-----------------------------------------------\n";
+    cout << "Ingrese el valor del exponente para base 10: ";
 
-	pthread_exit(NULL);
-	
-	return 0;
+    //Lectura desde stdin de un valor según el tipo de la variable destino
+    cin >> exponente;
+
+    cout << "Ingrese el numero de threads: ";
+    cin >> numThreads;
+
+    long limit = pow(10, exponente);
+
+    long i = 0;
+    int rc;
+
+    // Definición del objeto pthread
+    pthread_t threadID[numThreads];
+
+    // Contador de inicio en ticks de reloj
+    start_t = clock();
+
+    // Calcula cuántos números manejará cada hilo
+    long numbersPerThread = limit / numThreads;
+    long remainder = limit % numThreads;
+
+    for (int j = 0; j < numThreads; j++) {
+        ThreadData threadData;
+        threadData.start = i;
+        threadData.end = i + numbersPerThread + (j < remainder ? 1 : 0);
+
+ 		
+        rc = pthread_create(&threadID[j], NULL, primeTest, &threadData);
+		
+     	cout << "Buscando primos entre " << i << " y " << threadData.end - 1 << "...";
+        cout << endl;
+
+        if (rc) {
+            cout << "No se pudo crear el hilo " << j << endl;
+            exit(-1);
+        }
+        i = threadData.end;
+    }
+
+    for (int j = 0; j < numThreads; j++) {
+        rc = pthread_join(threadID[j], NULL);
+        if (rc) {
+            cout << "No se pudo unir el hilo " << j << endl;
+            exit(-1);
+        }
+    }
+
+    end_t = clock();
+    total_t = difftime(end_t, start_t) / CLOCKS_PER_SEC;
+    cout << "Total time: " << total_t << " seconds\n";
+
+    pthread_exit(NULL);
+
+    return 0;
 }
 
-/*
- * primeTest
- */
-void *primeTest (void *thisNumber)
-{
+// Implementación de la función que se ejecutará en cada hilo
+void *primeTest(void *threadDataPtr) {
+    ThreadData *threadData = static_cast<ThreadData *>(threadDataPtr);
 
-	long intNumber = (long)thisNumber;
+    for (long i = threadData->start; i < threadData->end; i++) {
+        if (isPrime(i)) {
+            cout << i << " from thread num: " << pthread_self() << endl;
+        }
+    }
 
-    //Acciones solamente si el numero es primo
-	if(isPrime(intNumber))
-	{
-		cout<<intNumber<<" from thread num: "<<pthread_self()<<endl;
-	}
-	pthread_exit(NULL);
+    pthread_exit(NULL);
 }
 
-/*
- * isPrime - function
- */
-bool isPrime (int number)
-{
-	if (number == 0 || number == 1)
-	{
-		return false;
-	}
-	int divisor;
-	for (divisor = number/2; number%divisor != 0; --divisor)
-	{
-		; //Algoritmo rapido para encontrar el residuo de un primo
-	}
-	if (divisor != 1)
-	{
-		return false;  //El residuo <> 1 si el numero NO es primo
-	}
-	else
-	{
-		return true;  //El residuo es 1 si el numero es primo
-	}
+// Función para verificar si un número es primo
+bool isPrime(int number) {
+    if (number <= 1) {
+        return false;
+    }
+
+    for (int divisor = 2; divisor * divisor <= number; divisor++) {
+        if (number % divisor == 0) {
+            return false;
+        }
+    }
+
+    return true;
 }
